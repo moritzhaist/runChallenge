7b968c198ea3dc9330e7889224fdc5ded776a7b8
Moritz Haist <mail@bildstrich.net>
2021-02-20 20:24:57 +0100

    [TASK] Add widget extension

---
+++ EXTRunChallengeWidget/EXTRunChallengeWidget.swift
@@ -0,0 +1,68 @@
+//
+//  EXTRunChallengeWidget.swift
+//  EXTRunChallengeWidget
+//
+//  Created by Moritz Haist on 20.02.21.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct EXTRunChallengeWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text("Es ist:")
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct EXTRunChallengeWidget: Widget {
+    let kind: String = "EXTRunChallengeWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            EXTRunChallengeWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct EXTRunChallengeWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        EXTRunChallengeWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
