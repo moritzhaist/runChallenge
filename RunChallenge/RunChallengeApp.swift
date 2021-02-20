f2b4e9ccf6b03190dec4d430c6d5374ab9f1682e
Moritz Haist <mail@bildstrich.net>
2021-02-20 18:55:40 +0100

    Initial Commit

---
+++ RunChallenge/RunChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunChallengeApp.swift
+//  RunChallenge
+//
+//  Created by Moritz Haist on 20.02.21.
+//
+
+import SwiftUI
+
+@main
+struct RunChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
