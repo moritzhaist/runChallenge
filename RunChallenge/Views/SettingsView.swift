81edf7deee3494e3ffe2b786c6057061a3bafd1b
Moritz Haist <mail@bildstrich.net>
2021-06-21 22:37:34 +0200

    [TASK] Rebuild settingsView

--- RunChallenge/Views/SettingsView.swift
+++ RunChallenge/Views/SettingsView.swift
@@ -14,85 +14,132 @@ struct SettingsView: View {
     
     func sliderValueChanged() {
         let selectionFeedback = UISelectionFeedbackGenerator()
-                       selectionFeedback.selectionChanged()
+        selectionFeedback.selectionChanged()
     }
-
-    var body: some View {
     
-            VStack() {
-                HStack(alignment: .top) {
-                    VStack(alignment: .leading, spacing: 2) {
-                        Text("Set your")
-                            .font(.system(.largeTitle, design: .default))
-                            .fontWeight(.black)
-                        Text("Challenge")
-                            .font(.system(.largeTitle, design: .default))
-                            .fontWeight(.black)
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                // header
+                VStack {
+                    ZStack(alignment: .topLeading) {
+                        Image("settingsHeaderRunning")
+                            .resizable()
+                            .scaledToFill()
+                            .overlay(ImageOverlay().opacity(0.7))
+                        // headline / button
+                        HStack(alignment: .top) {
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text("Set your")
+                                    .font(.system(.largeTitle, design: .default))
+                                    .fontWeight(.black)
+                                Text("Challenge")
+                                    .font(.system(.largeTitle, design: .default))
+                                    .fontWeight(.black)
+                            }
+                            .foregroundColor(.white)
+                            Spacer()
+                            Button(action: {
+                                isPresented = false
+                            }, label: {
+                                Image(systemName: "chevron.down.circle.fill")
+                                    .foregroundColor(Color.white)
+                                    .font(.largeTitle)
+                            })
+                        }
+                        .padding(20)
+                        // distance challenge
+                        ZStack(alignment: .bottom){
+                            Color.clear
+                            ZStack(alignment: .center) {
+                                Circle()
+                                    .stroke(
+                                        LinearGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.5354012868490564, saturation: 0.45592020793133475, brightness: 1.0, opacity: 1.0), location: 0.0), Gradient.Stop(color: Color(hue: 0.5639713241393308, saturation: 1.0, brightness: 1.0, opacity: 1.0), location: 0.9990679227388822)]), startPoint: UnitPoint.bottomTrailing, endPoint: UnitPoint.topLeading),
+                                        style: StrokeStyle(lineWidth: 2.0, lineCap: .round, lineJoin: .round))
+                                    .foregroundColor(Color.white)
+                                    .frame(width: 140, height: 140)
+                                    .padding(30)
+                                VStack {
+                                    Text("\(String(format: "%.0f", currentChallenge))")
+                                        .font(.system(size: 30.0, design: .default))
+                                        .fontWeight(.black)
+                                        .foregroundColor(.white)
+                                        .padding(.bottom, 1.0)
+                                    Text("KM")
+                                        .foregroundColor(.gray)
+                                        .font(.system(size: 15.0, weight: .regular, design: .default))
+                                }
+                            }
+                            .foregroundColor(.white)
+                        }
                     }
-                    Spacer()
-                    
-                    Button(action: {
-                            isPresented = false
-                        }, label: {
-                            Image(systemName: "chevron.down.circle.fill")
-                                .foregroundColor(Color.gray)
-                                .font(.largeTitle)
-                        })
-                
-                }
-                .padding(20)
-                
-                VStack{
-                    Spacer()
-                    Text("\(String(format: "%.0f", currentChallenge)) KM")
-                        .font(.system(size: 30.0, weight: .black, design: .default))
-                    Spacer()
-                    Text("Set the distance you want to run this month")
-                    
                 }
                 
-                HStack {
-                    Text(" 0\nkm")
-                        .padding()
-                        .font(.system(.caption))
-                    ZStack {
-                          LinearGradient(
-                              gradient: Gradient(colors: [.blue, .blue]),
-                              startPoint: .leading,
-                              endPoint: .trailing
-                          )
-                          .mask(Slider(value: $currentChallenge, in: 0...300, step: 1))
-                          Slider(value: Binding(
+                // slider & button
+                VStack {
+                    
+                    VStack {
+                        Spacer()
+                        Text("How many kilometers do you want to run this month?")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding(.horizontal, 20)
+                    }
+                    ZStack(alignment: .center) {
+                        LinearGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.5354012868490564, saturation: 0.45592020793133475, brightness: 1.0, opacity: 1.0), location: 0.0), Gradient.Stop(color: Color(hue: 0.5639713241393308, saturation: 1.0, brightness: 1.0, opacity: 1.0), location: 0.9990679227388822)]), startPoint: UnitPoint.bottomTrailing, endPoint: UnitPoint.topLeading)
+                            .mask(Slider(value: $currentChallenge, in: 0...300, step: 1))
+                        Slider(value: Binding(
                             get: {
                                 self.currentChallenge
                             },
                             set: {(newValue) in
-                                  self.currentChallenge = newValue
-                                  self.sliderValueChanged()
+                                self.currentChallenge = newValue
+                                self.sliderValueChanged()
                             }
-                        ), in: 0...300, step: 1)
-                              .accentColor(.clear)
+                        ), in: 1...300, step: 1)
+                        .accentColor(.clear)
                     }
-                    Text("300\n km")
-                        .padding()
-                        .font(.system(.caption))
-                }
-        
-         
-                
-                Button("Accept Challenge", action: {self.updateChallenge()} )
-                    .padding()
-                    .background(Color/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    
                     .foregroundColor(.white)
-                    .border(Color.blue, width: 5)
-                    .cornerRadius(40)
-                Spacer()
+                    .padding(20)
+                    
+                    Button(action: {
+                        self.updateChallenge()
+                    }) {
+                        Text("Set challenge")
+                            .fontWeight(.medium)
+                            .font(.body)
+                            .padding()
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 40)
+                                    .stroke(LinearGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.5354012868490564, saturation: 0.45592020793133475, brightness: 1.0, opacity: 1.0), location: 0.0), Gradient.Stop(color: Color(hue: 0.5639713241393308, saturation: 1.0, brightness: 1.0, opacity: 1.0), location: 0.9990679227388822)]), startPoint: UnitPoint.bottomTrailing, endPoint: UnitPoint.topLeading), lineWidth: 2)
+                            )
+                    }
+                }
+                
+            }
         }
     }
     func updateChallenge() {
         let newChallenge = currentChallenge
         UserDefaults(suiteName: "group.com.bildstrich.net.RunChallenge")!.set(newChallenge, forKey: "distanceChallenge")
         isPresented = false
+    }
+    
+}
+
+struct ImageOverlay: View {
+    var body: some View {
+        ZStack {
+            Text(" ")
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(Color.black)
     }
 }
 
