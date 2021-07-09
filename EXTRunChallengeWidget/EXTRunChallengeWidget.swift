094f533ab5b72ec3507007f720e2ec63db36a9ad
Moritz Haist <mail@bildstrich.net>
2021-07-09 17:23:17 +0200

    [TASK] Create basic small widget

--- EXTRunChallengeWidget/EXTRunChallengeWidget.swift
+++ EXTRunChallengeWidget/EXTRunChallengeWidget.swift
@@ -13,29 +13,33 @@ struct Provider: TimelineProvider {
     @AppStorage("sumOfRunsMonth", store: UserDefaults(suiteName:"group.com.bildstrich.net.RunChallengeApp"))
     var value: Double = Double()
     
+    // updated in SettingsView
+    @AppStorage("distanceChallenge", store: UserDefaults(suiteName:"group.com.bildstrich.net.RunChallengeApp"))
+    var distance: Double = 50.00
+    
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), value: value)
+        SimpleEntry(date: Date(), value: value, distance: distance)
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), value: value)
+        let entry = SimpleEntry(date: Date(), value: value, distance: distance)
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         
         
         
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, value: value)
+            let entry = SimpleEntry(date: entryDate, value: value, distance: distance)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -44,37 +48,72 @@ struct Provider: TimelineProvider {
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let value: Double
+    let distance: Double
     
 }
 
-struct RunTrackerWidgetEntryView : View {
+struct RunChallengeWidgetEntryView : View {
     
-   
-
+    
+    
     var entry: Provider.Entry
     
-
-    var body: some View {
-        Text("👟")
-        Text("")
-        Text("\(String(format: "%.3f", entry.value)) km")
-        
-    }
     
-   
+    var body: some View {
+        
+        ZStack(alignment: .top) {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                // circle
+                VStack {
+                    ZStack {
+                        Circle()
+                            .trim(from: 0.0, to: CGFloat(min(0.75, 1.0)))
+                            .stroke(
+                                LinearGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color("mLightBlue"), location: 0.0), Gradient.Stop(color: Color("mDarkBlue"), location: 0.9990679227388822)]), startPoint: UnitPoint.bottomTrailing, endPoint: UnitPoint.topLeading),
+                                style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
+                            .rotationEffect(Angle(degrees: 270.0))
+                            .frame(width: 70, height: 70, alignment: .center)
+                            .padding(5)
+                        Spacer()
+                        Image("shoeIcon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                    }
+                    .padding(10)
+                }
+                .background(Color(".red"))
+                Spacer()
+               
+                VStack(alignment: .center) {
+                    Spacer()
+                    Text("\(String(format: "%.0f", entry.value)) KM / \(String(format: "%.0f", entry.distance)) KM")
+                        .foregroundColor(.white)
+                        .font(.system(size: 17.0, weight: .regular, design: .default))
+                    Spacer()
+                }
+            }
+            .padding(10)
+        }
+    }
 }
+
+
 
 @main
 
 struct RunTrackerWidget: Widget {
     
     
-
     let kind: String = "RunTrackerWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            RunTrackerWidgetEntryView(entry: entry)
+            RunChallengeWidgetEntryView(entry: entry)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.green)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
@@ -82,3 +121,9 @@ struct RunTrackerWidget: Widget {
     
 }
 
+struct EXTRunChallengeWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        RunChallengeWidgetEntryView(entry: SimpleEntry(date: Date(), value: 120.00, distance: 200.00))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
