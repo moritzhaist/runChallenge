8b456c25101539952649058e67fec0a448c0d224
Moritz Haist <mail@bildstrich.net>
2021-08-01 16:02:00 +0200

    [TASK] Update Version and build number

--- RunChallenge/Views/InfoView.swift
+++ RunChallenge/Views/InfoView.swift
@@ -106,7 +106,7 @@ struct InfoView: View {
                         HStack {
                             Image(systemName: "app.badge")
                                 .padding(.trailing)
-                            Text("Version 1.0.0 beta 2")
+                            Text("Version 1.0.0 beta 3")
                             Spacer()
                         }
                         .modifier(ctaModifier())
